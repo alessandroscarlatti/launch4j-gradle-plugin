e49bf5e1cc1085939cd017777e1bb29ba3189cbe
alessandroscarlatti <violanotesnoextras@gmail.com>
2018-05-03 20:29:05 -0500

    add plugin for test project builds

---
+++ buildSrc/src/main/groovy/com/scarlatti/test/util/TestGradleBuildPlugin.groovy
@@ -0,0 +1,79 @@
+package com.scarlatti.test.util
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.tasks.GradleBuild
+
+import java.nio.file.Paths
+
+/**
+ * ______    __                         __           ____             __     __  __  _
+ * ___/ _ | / /__ ___ ___ ___ ____  ___/ /______    / __/______ _____/ /__ _/ /_/ /_(_)
+ * __/ __ |/ / -_|_-<(_-</ _ `/ _ \/ _  / __/ _ \  _\ \/ __/ _ `/ __/ / _ `/ __/ __/ /
+ * /_/ |_/_/\__/___/___/\_,_/_//_/\_,_/_/  \___/ /___/\__/\_,_/_/ /_/\_,_/\__/\__/_/
+ * Thursday, 5/3/2018
+ */
+class TestGradleBuildPlugin implements Plugin<Project> {
+
+    private Project project
+
+    @Override
+    void apply(Project project) {
+        // read a list of test project dirs from the
+        // gradle project property
+        this.project = project
+
+        createTestPluginProjectTask()
+        createTestPluginProjectTasks()
+    }
+
+    private void createTestPluginProjectTasks() {
+        List<String> testProjectDirs = getTestProjectDirs()
+        println "Considering testProjectDirs ${testProjectDirs}"
+
+        for (String dir : testProjectDirs) {
+            createTestGradleBuildTask(dir)
+        }
+    }
+
+    private List<String> getTestProjectDirs() {
+        Object rawTestProjectDirs = project.gradle.testProjectDirs // project.getProperties().get("testProjectDirs")
+
+        if (!(rawTestProjectDirs instanceof List))
+            throw new IllegalArgumentException("testProjectDirs property should be a list of test project dirs (strings).")
+
+        return (List<String>) rawTestProjectDirs
+    }
+
+    private void createTestPluginProjectTask() {
+        project.tasks.create("testPluginProjects") { task ->
+            task.group = 'verification'
+            task.description = 'Tests this plugin in test projects.'
+            task.mustRunAfter(project.tasks.getByName("build"))
+            task.outputs.upToDateWhen {false}
+        }
+
+        project.tasks.getByName("test").finalizedBy(project.tasks.getByName("testPluginProjects"))
+    }
+
+    private void createTestGradleBuildTask(String rawDir) {
+
+        String dirTaskName = Paths.get(rawDir).toString().replace(File.separatorChar, '_' as char)
+        String taskName = "test_" + dirTaskName
+        println "Creating task named ${taskName}"
+
+        project.tasks.create(taskName, GradleBuild.class) { build ->
+            build.mustRunAfter(project.tasks.getByName('build'))
+            build.dir = Paths.get(project.projectDir.absolutePath, rawDir).toString()
+            build.tasks = ['testPluginProject']
+            build.startParameter.projectProperties.putAll([
+                    group:  project.group,
+                    version: project.version,
+                    repoDir: Paths.get(project.projectDir.absolutePath, project.properties.pluginRepoDir),
+                    pluginName: project.properties.pluginName
+            ])
+        }
+
+        project.tasks.test.finalizedBy(project.tasks.getByName(taskName))
+    }
+}
